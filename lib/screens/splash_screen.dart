e69bc093de8141c83f672d96e64694ff2113e6dd
Mohammad Abdullah <mabdullahm773@gmail.com>
2025-05-10 02:19:13 +0500

    Splashscreen Updated

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,29 @@
+import 'package:lottie/lottie.dart';
+
+import 'home_screen.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 1),(){
+      Navigator.push(context, MaterialPageRoute(builder: (_) => Homescreen()));
+    });
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        child: Lottie.asset('animations/splashscreen.json'),
+      ),
+    );
   }
 }
